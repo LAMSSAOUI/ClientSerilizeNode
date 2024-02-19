7433c7f101b3dc97ffe92498b6fafdcde7fa67fd
LAMSSAOUI ABDELALI <lam998216@gmail.com>
2024-02-19 16:15:29 +0100

    prouving two vulnerabilities

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,28 @@
 'use client'
-import { useState } from "react";
-import serialize from "node-serialize"; // Make sure to install node-serialize package
+import { useState , useEffect } from "react";
+import serialize from "node-serialize";
+import Cookies from "js-cookie"; // Import the js-cookie library
+import { useRouter } from 'next/navigation'
+
 
 export default function Home() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [userData, setUserData] = useState(null);
+
+  const router = useRouter()
+  useEffect(() => {
+    if (userData && userData.length > 0) {
+      router.push('/admin');
+    }
+  }, [userData, router]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     
-    // {
-    //   "username": "_$$ND_FUNC$$_function(){ return 'Hello'; }()",
-    //   "gender": "Male",
-    //   "Age": 40
-    // };
-
     // Serialize the data
     const serializedData = serialize.serialize({ username, password });
-
-    console.log(serializedData)
-
+   
     try {
       // Send serialized data to the server
       const response = await fetch('http://localhost:3000/api/login', {
@@ -28,13 +31,19 @@ export default function Home() {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({ data: serializedData })
-
       });
-      // body: JSON.stringify({ data: serializedData })
 
       if (response.ok) {
+        // Save username and password in cookies
+        Cookies.set('username', username);
+        Cookies.set('password', password);
+
         // Handle success
         console.log('Data sent successfully');
+
+        const jsonData = await response.json();
+        setUserData(jsonData); 
+        console.log('the user data from the server is ' , userData);
       } else {
         // Handle error
         console.error('Failed to send data to the server');
@@ -45,30 +54,41 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <h2>Generate Credentials</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-row gap-4">
-          <label>Username:</label>
-          <input 
-            type="text" 
-            value={username} 
-            onChange={(e) => setUsername(e.target.value)} 
-            className="border-2"
-          />
+
+    <div className="flex justify-center items-center h-screen ">
+      <div className="flex max-w-2xl mx-auto shadow border-b  flex-col gap-4 w-fit p-10">
+          <h2 className="text-center font-thin  text-2xl tracking-wider">Login</h2>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <div className="flex flex-col  gap-4">
+              <label className="block text-gray-600 text-sm  font-normal ">Username:</label>
+              <input 
+                type="text" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+                className=" border h-10 w-96 mt-2 p-2"
+              />
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="block text-gray-600 text-sm  font-normal ">Password:</label>
+              <input 
+                type="text" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                className="border h-10 w-96 mt-2  p-2"
+              />
+            </div>
+            <button type="submit" className=" rounded text-white font-semibold  mr-3 bg-green-400  hover:bg-green-600 px-2 py-2 w-full" >Submit</button>
+          </form>
+      </div>
+
+      {/* {userData && (
+        <div>
+          <h3>User Data:</h3>
+          <pre>{JSON.stringify(userData, null, 2)}</pre>
         </div>
-        <div className="flex flex-row gap-4">
-          <label>Password:</label>
-          <input 
-            type="text" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            className="border-2"
-          />
-        </div>
-        <button type="submit">Generate New Credentials</button>
-      </form>
+      )} */}
     </div>
   )
 }
+
 
---
+++ src/pages/admin.js
@@ -0,0 +1,9 @@
+export default function Admin() {
+  return (
+    <div style={{ height: '97vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div style={{backgroundColor: '#34D399', padding: '1.9rem', color: 'white' , fontSize: '1.5rem' }}>
+         Hello to the Admin Page
+      </div>
+    </div>
+  );
+}
