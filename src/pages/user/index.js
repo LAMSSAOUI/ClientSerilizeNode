1b6b383b8ad99471b490a840a2da472a747d348c
LAMSSAOUI ABDELALI <lam998216@gmail.com>
2024-02-29 23:26:56 +0100

    the client able to add a note vulnerable

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -3,6 +3,35 @@ import React, { useState, useEffect } from 'react';
 const index = () => {
   const [imageUrl, setImageUrl] = useState('');
 
+  const [note, setNote] = useState('');
+
+  const handleChange = (event) => {
+    setNote(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Serialize data
+    const data = { imageUrl: imageUrl, note: note };
+
+    // Send data via fetch POST
+    fetch('http://localhost:3000/api/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response)
+      .then(data => {
+        console.log('Success:', data);
+        // Handle success if needed
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        // Handle error if needed
+      });
+  };
+
   useEffect(() => {
     // Function to fetch the image from the server
     const fetchImage = async () => {
@@ -29,8 +58,23 @@ const index = () => {
   }, []); // Empty dependency array ensures this effect runs only once
 
   return (
-    <div>
-      {imageUrl && <img src={imageUrl} alt="Fetched Image" />}
+    <div className='flex flex-col justify-center'>
+        <div className='flex justify-center'>Make a note on this image</div>
+
+        <div className='flex  justify-center'>
+            <div className='w-96 h-40 '>
+            {imageUrl && <img src={imageUrl} alt="Fetched Image" />}
+            </div>
+        </div>
+         <input type='text' value={note} onChange={handleChange}  className='border-dotted border-red'/>
+         <input 
+                type="text" 
+                value={note} 
+                onChange={handleChange}
+                className="border h-10 w-96 mt-2  p-2"
+        />
+
+        <button onClick={handleSubmit}>Envoyer</button>
     </div>
   );
 };
